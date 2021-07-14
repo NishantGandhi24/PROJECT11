5019b163bfec10203772d89b66f8c8eb5468c57e
NishantGandhi24 <86958159+NishantGandhi24@users.noreply.github.com>
2021-07-14 20:04:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,23 +1,36 @@
-var boy,boy_running
-var path,path_image
+var boy,boy_running;
+var path,path_image;
+var invisible_groundR;
+var invisible_groundL;
+
 function preload(){
   //pre-load images
   boy_running = loadAnimation("Runner-1.png","Runner-2.png");
   path_image = loadImage("path.png");
-  
-}
+  }
 
 function setup(){
-  createCanvas(1000,1000);
+  createCanvas(1000,900);
   //create sprites here
-  boy = createSprite(100,200,500,500)
-  boy.addAnimation("runner",boy_running);
-  boy.scale=5;
-  path = createSprite(500,500,40,100);
+  path = createSprite(500,500,1000,100);
   path.addImage("path",path_image);
-}
+  boy = createSprite(590,600,100,100)
+  boy.addAnimation("runner",boy_running);
+  boy.scale=0.1;
+  invisible_groundR=createSprite(1000,500,700,900);
+  invisible_groundR.visible=false;
+  invisible_groundL=createSprite(160,500,400,900);
+  invisible_groundL.visible=false;
+  }
 
 function draw() {
   background("white");
-
+  path.velocityY=10;
+  if(path.y>900){
+  path.y = height/2;
+  boy.x =mouseX;
+  boy.collide(invisible_groundR);
+  boy.collide(invisible_groundL);
+}
+  drawSprites();
 }
