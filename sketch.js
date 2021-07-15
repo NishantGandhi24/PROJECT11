eb446104f970e8a1138682705ee197572205f859
NishantGandhi24 <86958159+NishantGandhi24@users.noreply.github.com>
2021-07-15 16:01:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,9 +17,9 @@ function setup(){
   boy = createSprite(590,600,100,100)
   boy.addAnimation("runner",boy_running);
   boy.scale=0.1;
-  invisible_groundR=createSprite(1000,500,700,900);
+  invisible_groundR=createSprite(1000,500,100,900);
   invisible_groundR.visible=false;
-  invisible_groundL=createSprite(160,500,400,900);
+  invisible_groundL=createSprite(160,500,100,900);
   invisible_groundL.visible=false;
   }
 
@@ -28,9 +28,10 @@ function draw() {
   path.velocityY=10;
   if(path.y>900){
   path.y = height/2;
+  }
   boy.x =mouseX;
   boy.collide(invisible_groundR);
   boy.collide(invisible_groundL);
-}
-  drawSprites();
+  
+drawSprites();
 }
